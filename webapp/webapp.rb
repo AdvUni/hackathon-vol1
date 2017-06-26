e8525c19512c28ec51fcfe1538ae9a042a12df21
Michael Drüing <michael.drueing@advanced-unibyte.de>
2017-06-26 12:40:17 +0200

    Revert to unirest for now

--- webapp/webapp.rb
+++ webapp/webapp.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
+require 'unirest'
 require 'elasticsearch'
-require 'rest-client'
+#require 'rest-client'
 
 es_config = {host: ENV['ELASTICSEARCH_HOST']}
 es = Elasticsearch::Client.new(es_config)
+
+Unirest.timeout(20)
 
 # Show welcome page
 get "/" do
@@ -30,7 +33,8 @@ end
 # Show take photo page
 get "/take_photo/:camera_ip" do
   camera_ip = params[:camera_ip]
-  response = RestClient::Request.execute(method: :post, url: "http://#{camera_ip}:8080/take_photo", timeout: 10)
+  #response = RestClient::Request.execute(method: :post, url: "http://#{camera_ip}:8080/take_photo", timeout: 20)
+  response = Unirest.post "http://#{camera_ip}:8080/take_photo"  
   @image_url = response.body['url'].to_s
   @timestamp = response.body['timestamp'].to_s
 
